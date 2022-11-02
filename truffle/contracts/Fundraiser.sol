48e97a1011154fcd915c514206725afc88ca4d39
chucker34 <kawakami34@gmail.com>
2022-11-02 20:34:17 +0900

    6-4まで

--- truffle/contracts/Fundraiser.sol
+++ truffle/contracts/Fundraiser.sol
@@ -3,8 +3,19 @@ pragma solidity >0.4.23;
 
 contract Fundraiser {
   string public name;
+  string public url;
+  string public imageURL;
+  string public description;
 
-  constructor(string memory _name) {
+  constructor(
+    string memory _name,
+    string memory _url,
+    string memory _imageURL,
+    string memory _description
+  ) {
     name = _name;
+    url = _url;
+    imageURL = _imageURL;
+    description = _description;
   }
 }
