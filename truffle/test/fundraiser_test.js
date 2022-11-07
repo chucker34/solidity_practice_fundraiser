a6660aad22d886d30d63824c7a5bab8c924b83e8
chucker34 <kawakami34@gmail.com>
2022-11-07 19:39:34 +0900

    6-7まで

--- truffle/test/fundraiser_test.js
+++ truffle/test/fundraiser_test.js
@@ -6,6 +6,8 @@ contract('Fundraiser', accounts => {
   const url = 'beneficiaryname.org'
   const imageURL = 'https://placekitten.com/600/350'
   const description = 'Beneficiary description'
+  const beneficiary = accounts[1]
+  const custodian = accounts[0]
 
   describe('initialization', () => {
     beforeEach(async () => {
@@ -13,7 +15,9 @@ contract('Fundraiser', accounts => {
         name,
         url,
         imageURL,
-        description
+        description,
+        beneficiary,
+        custodian
       )
     })
 
@@ -35,6 +39,16 @@ contract('Fundraiser', accounts => {
     it('gets the beneficiary description', async () => {
       const actual = await fundraiser.description()
       assert.equal(actual, description, 'description should match')
+    })
+
+    it('gets the beneficiary', async () => {
+      const actual = await fundraiser.beneficiary()
+      assert.equal(actual, beneficiary, 'beneficiary addresses should match')
+    })
+
+    it('gets the custodian', async () => {
+      const actual = await fundraiser.custodian()
+      assert.equal(actual, custodian, 'custodian should match')
     })
   })
 })
