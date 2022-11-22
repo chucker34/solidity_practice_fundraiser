8e0ab1f0b4da1acd325dba36a9d2bd614b47a206
chucker34 <kawakami34@gmail.com>
2022-11-22 23:27:43 +0900

    6-29まで

--- truffle/contracts/Fundraiser.sol
+++ truffle/contracts/Fundraiser.sol
@@ -81,4 +81,9 @@ contract Fundraiser is Ownable {
     beneficiary.transfer(balance);
     emit Withdraw(balance);
   }
+
+  fallback () external payable {
+    totalDonations = totalDonations.add(msg.value);
+    donationsCount++;
+  }
 }
