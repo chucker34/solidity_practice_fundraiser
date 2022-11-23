e0a59451d4553511f21046340c8d0c2e95753fbe
chucker34 <kawakami34@gmail.com>
2022-11-23 17:28:18 +0900

    7-5まで

--- truffle/contracts/FundraiserFactory.sol
+++ truffle/contracts/FundraiserFactory.sol
@@ -1,7 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >0.4.23;
 
+import "./Fundraiser.sol";
 
 contract FundraiserFactory {
+  Fundraiser[] private _fundraisers;
 
+  function createFundraiser(
+    string memory name,
+    string memory url,
+    string memory imageURL,
+    string memory description,
+    address payable beneficiary
+  )
+    public
+  {
+    Fundraiser fundraiser = new Fundraiser(
+      name,
+      url,
+      imageURL,
+      description,
+      beneficiary,
+      msg.sender
+    );
+    _fundraisers.push(fundraiser);
+  }
+
+  function fundraisersCount() public view returns(uint256) {
+    return _fundraisers.length;
+  }
 }
